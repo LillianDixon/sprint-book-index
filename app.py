2c75c7066be561244a09d393e0d7925083501b29
Lillian Dixon <dixon.lillian92@gmail.com>
2020-03-20 15:31:48 -0600

    3 20 2020

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_heroku import Heroku
 
 app = Flask(__name__)
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://pliqxsxtwnkgil:9327b81bfb7273e92514c5ed35b5385109cf972b6bb8200a0de8a05028b753be@ec2-18-209-187-54.compute-1.amazonaws.com:5432/deh7ps5dduurdu"
 
 heroku = Heroku(app)
 db = SQLAlchemy(app)
@@ -27,6 +27,51 @@ class Book(db.Model):
 @app.route('/')
 def home():
     return "<h1>Hi from Flask</h1>"
+
+@app.route('/book/input', methods=['POST'])
+def books_input():
+    if request.content_type == 'application/json':
+        post_data = request.get_json()
+        title = post_data.get('title')
+        author = post_data.get('author')
+        reg = Book(title, author)
+        db.session.add(reg)
+        db.session.commit()
+        return jsonify('Data Posted')
+    return jsonify('Something went terribly wrong')
+
+@app.route('/books', methods = ['GET'])
+def return_books():
+    all_books = db.session.query(Book.id, Book.title, Book.author).all()
+    return jsonify(all_books)
+
+@app.route('/book/<id>', methods=['GET'])
+def return_single_book(id):
+    one_book = db.session.query(Book.id, Book.title, Book.author).filter(Book.id == id).first()
+    return jsonify(one_book)
+
+@app.route('/delete/<id>', methods=['DELETE'])
+def book_delete(id):
+    if request.content_type == 'application/json':
+        record = db.session.query(Book).get(id)
+        db.session.delete(record)
+        db.session.commit()
+        return jsonify('completed Delete action')
+    return jsonify('Delete failed')
+
+@app.route('/update_book/<id>', methods=['PUT'])
+def book_update(id):
+    if request.content_type == 'application/json':
+        put_data = request.get_json()
+        title = put_data.get('title')
+        author = put_data.get('author')
+        record = db.session.query(Book).get(id)
+        record.title = title
+        record.author = author
+        db.session.commit()
+        return jsonify('Completed Update')
+    return jsonify('Failed Update')
+
 
 if __name__ == '__main__':
     app.debug = True
